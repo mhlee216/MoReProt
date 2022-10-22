06c19b4bbc39cf6ae0ef644adc8f0d64b7bda93b
mhlee <leemh216@gmail.com>
2022-10-22 23:49:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(name='moreprot', 
       version="1.0.0", 
       url='https://github.com/mhlee216/MoReProt', 
@@ -7,6 +10,6 @@ setup(name='moreprot',
       author='Myeonghun Lee', 
       author_email="leemh216@gmail.com", 
       description='Molecular Fingerprint Recombination-based Protein Fingerprint', 
-      long_description="Molecular Fingerprint Recombination-based Protein Fingerprint.", 
+      long_description=long_description, 
       install_requires=["numpy >= 1.19.0", "rdkit >= 2021.09.2"],
-      classifiers=['License :: OSI Approved :: MIT License'])
+      classifiers=['Programming Language :: Python :: 3', 'License :: OSI Approved :: MIT License'])
